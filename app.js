869af900d9ba9076de1b7f16b189aebea10879cf
Yash <86472828+xsupremeyx@users.noreply.github.com>
2026-04-13 15:41:18 +0000

    feat: import necessaties and swt .env up.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+require("dotenv").config();
 
 // ejs and styles imports
 app.set("views", path.join(__dirname,"views"));
